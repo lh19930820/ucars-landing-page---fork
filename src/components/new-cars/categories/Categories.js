325aaff3cd72eecab51cc00ad4368ed58a2a2a1d
unknown <kimthanh932000@gmail.com>
2022-09-29 17:46:38 +0700

    +

--- src/components/new-cars/categories/Categories.js
+++ src/components/new-cars/categories/Categories.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Categories.css';
 import State from './state/State';
 import Price from './price/Price';
 import Type from './type/Type';
 
 const Categories = () => {
+  const [showStateDropdown, setShowStateDropdown] = useState(false);
+  const [showPriceDropdown, setShowPriceDropdown] = useState(false);
+  const [showTypeDropdown, setShowTypeDropdown] = useState(false);
+
+
   return (
     <div className='categories'>
       <form action="#" className='search-form'>
-        <div className='category'>
+        <div className={`category ${showStateDropdown && 'active'}`} onClick={() => setShowStateDropdown(!showStateDropdown)}>
           <p className='category__title'>New/ Used </p>
           <span className='category__placeholder'>New Cars</span>
-          <State/>
+          {
+            showStateDropdown && <State />
+          }
         </div>
-        <div className='category'>
+        <div className={`category ${showPriceDropdown && 'active'}`} onClick={() => setShowPriceDropdown(!showPriceDropdown)}>
           <p className='category__title'>Price Range </p>
           <span className='category__placeholder'>
-            <span className="min">10,0000</span> --
-            <span className="max">100,0000</span>
+            <span className="min">&#36;10,0000</span>
+            <span className="max">&#36;100,0000</span>
           </span>
-          <Price/>
+          {
+            showPriceDropdown && <Price />
+          }
         </div>
-        <div className='category'>
+        <div className={`category ${showTypeDropdown && 'active'}`} onClick={() => setShowTypeDropdown(!showTypeDropdown)}>
           <p className='category__title'>Vehicle Type </p>
           <span className='category__placeholder'>Duplex</span>
-          <Type/>
+          {
+            showTypeDropdown && <Type />
+          }
         </div>
         <button type='submit' className='category__btn btn--primary u-border-radius-sm'>Search</button>
       </form>
