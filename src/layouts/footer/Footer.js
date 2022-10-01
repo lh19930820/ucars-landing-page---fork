1ee6e9349b6eb2c6d15491b2357f12c7edf27604
unknown <kimthanh932000@gmail.com>
2022-10-01 20:14:40 +0700

    +

--- src/layouts/footer/Footer.js
+++ src/layouts/footer/Footer.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Footer.scss';
 import carBuyerLogoPc from 'assets/images/logo/carbuyer_logo_footer_pc.svg';
+import carBuyerLogoSP from 'assets/images/logo/carbuyer_logo_footer_sp.svg';
 import ucarLogo from 'assets/images/logo/ucars_logo_main.svg';
 import phoneIcon from 'assets/images/icons/phone.svg';
 import envelopeIcon from 'assets/images/icons/envelope.svg';
@@ -10,45 +11,83 @@ import fbIcon from 'assets/images/icons/fb.svg';
 import airplaneIcon from 'assets/images/icons/airplane.svg';
 import inIcon from 'assets/images/icons/in.svg';
 import youtubeIcon from 'assets/images/icons/youtube.svg';
-
+import useIsMobile from 'hooks/useIsMobile';
+import data from './_data/data';
 
 const Footer = () => {
+    const isMobile = useIsMobile();
+    const [menuItems, setMenuItems] = useState(data);
+
+    const toggleMenu = (e, targetPos) => {
+        if (isMobile) {
+            setMenuItems(menuItems.map((menu, i) => {
+                return {
+                    ...menu,
+                    active: targetPos === i && !menu.active
+                }
+            }))
+        }
+    }
+
+    useEffect(() => {
+        if (!isMobile) {
+            setMenuItems(menuItems.map(item => ({
+                ...item,
+                active: true
+            })))
+        } else {
+            setMenuItems(menuItems.map(item => ({
+                ...item,
+                active: false
+            })))
+        }
+    }, [isMobile])
+
     return (
         <footer className='l-footer'>
             <div className="container">
                 <div className='top'>
                     <div className='contact'>
-                        <img src={carBuyerLogoPc} alt="" />
+                        <img src={isMobile ? carBuyerLogoSP : carBuyerLogoPc} alt="" />
                         <ul>
-                            <li className='contact__item'><img className='contact__icon' src={markerIcon} alt="" width="11" />71 Ayer Rajah Crescent, #06-14, Singapore 139951</li>
-                            <li className='contact__item'><img className='contact__icon' src={phoneIcon} alt="" width="12" />+65 8808 7905</li>
-                            <li className='contact__item'><img className='contact__icon' src={envelopeIcon} alt="" width="13" />hello@carbuyer.com.sg</li>
+                            <li className='contact__item'>
+                                <img className='contact__icon' src={markerIcon} alt="" width="11" />
+                                <span>71 Ayer Rajah Crescent, #06-14, Singapore 139951</span>
+                            </li>
+                            <li className='contact__item'>
+                                <img className='contact__icon' src={phoneIcon} alt="" width="12" />
+                                <span> +65 8808 7905</span>
+
+                            </li>
+                            <li className='contact__item'>
+                                <img className='contact__icon' src={envelopeIcon} alt="" width="13" />
+                                <span>hello@carbuyer.com.sg</span>
+                            </li>
                         </ul>
                     </div>
-                    <div className='menu'>
-                        <span className='u-fw-600'>About</span>
-                        <ul>
-                            <li className='menu__item'><a href="#">About Us</a></li>
-                            <li className='menu__item'><a href="#">Contact Us</a></li>
-                            <li className='menu__item'><a href="#">Careers</a></li>
-                        </ul>
-                    </div>
-                    <div className='menu'>
-                        <span className='u-fw-600'>Services</span>
-                        <ul>
-                            <li className='menu__item'><a href="#">Buy Used Cars</a></li>
-                            <li className='menu__item'><a href="#">Buy New Cars</a></li>
-                            <li className='menu__item'><a href="#">Be a Dealer with UCARS</a></li>
-                        </ul>
-                    </div>
-                    <div className='menu'>
-                        <span className='u-fw-600'>Resources</span>
-                        <ul>
-                            <li className='menu__item'><a href="#">News</a></li>
-                            <li className='menu__item'><a href="#">Sell My Car</a></li>
-                            <li className='menu__item'><a href="#">COE Prices</a></li>
-                        </ul>
-                    </div>
+                    {
+                        menuItems.map((menu, i) => {
+                            if (menu.title !== 'Legal' || isMobile) {
+                                return (
+                                    <div className='menu' onClick={(e) => toggleMenu(e, i)}>
+                                        <div className='u-fw-600 menu__title'>{menu.title}</div>
+                                        {
+                                            // menu.active && <ul className='menu-list'>
+                                            //     {
+                                            //         menu.items.map(item => <li className='menu__item'><a href="#">{item}</a></li>)
+                                            //     }
+                                            // </ul>
+                                            <ul className={`menu-list ${menu.active ? 'u-d-block' : 'u-d-none'}`}>
+                                                {
+                                                    menu.items.map(item => <li className='menu__item'><a href="#">{item}</a></li>)
+                                                }
+                                            </ul>
+                                        }
+                                    </div>
+                                )
+                            }
+                        })
+                    }
                     <div className='subscription'>
                         <span>Get the <a href="#" className='u-text-red'>latest</a> automotive news sent to your inbox!</span>
                         <form action="#" className='p-sub-form'>
@@ -68,21 +107,28 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className='bottom'>
-                    <div className='legal'>
-                        {/* <span className='u-fw-600'>Legal</span> */}
-                        <ul>
-                            <li className='legal__item'><a href="#" className='legal__link'>Advertising Terms and Conditions</a></li>
-                            <li className='legal__item'><a href="#" className='legal__link'>Platform Terms and Conditions</a></li>
-                            <li className='legal__item'><a href="#" className='legal__link'>Privacy Policy</a></li>
-                        </ul>
-                        <div className='copyright'>
+                    {
+                        !isMobile &&
+                        <div className='legal'>
+                            <ul>
+                                <li className='legal__item'><a href="#" className='legal__link'>Advertising Terms and Conditions</a></li>
+                                <li className='legal__item'><a href="#" className='legal__link'>Platform Terms and Conditions</a></li>
+                                <li className='legal__item'><a href="#" className='legal__link'>Privacy Policy</a></li>
+                            </ul>
+                            <div className='copyright'>
+                                <p>© 2022. All rights reserved.</p>
+                            </div>
+                        </div>
+                    }
+                    <div className='ownership'>
+                        <a href="#"><img src={ucarLogo} alt="" width="55" /></a>
+                        <p>CarBuyer Pte Ltd and the CarBuyer Singapore brand are wholly owned by UCARS Pte Ltd</p>
+                    </div>
+                    {
+                        isMobile && <div className='copyright'>
                             <p>© 2022. All rights reserved.</p>
                         </div>
-                    </div>
-                    <div className='ownership'>
-                        <a href="#"><img src={ucarLogo} alt="" width="55"/></a>
-                        CarBuyer Pte Ltd and the CarBuyer Singapore brand are wholly owned by UCARS Pte Ltd
-                    </div>
+                    }
                 </div>
             </div>
         </footer>
---
+++ src/layouts/footer/_data/data.js
@@ -0,0 +1,22 @@
+export default [
+    {
+       title: 'About',
+       items: ['About Us', 'Contact Us', 'Careers'],
+       active: false
+    },
+    {
+        title: 'Services',
+        items: ['Buy Used Cars', 'Buy New Cars', 'Be a Dealer with UCARS'],
+        active: false
+     },
+     {
+        title: 'Resources',
+        items: ['News', 'Sell My Car', 'COE Prices'],
+        active: false
+     },
+     {
+        title: 'Legal',
+        items: ['Advertising Terms and Conditions', 'Platform Terms and Conditions', 'Privacy Policy'],
+        active: false
+     }
+]
