4fe1ed497dfac9c9fa181fb778c400085e8ff8ed
unknown <kimthanh932000@gmail.com>
2022-10-01 20:34:21 +0700

    +

--- src/layouts/header/navbar/Navbar.js
+++ src/layouts/header/navbar/Navbar.js
@@ -9,10 +9,10 @@ const Navbar = () => {
                 <div className='navbar__nav'>
                     <a href="#" className='logo pc'><img src={logoPC} alt="logo" /></a>
                     <ul className='navbar-list'>
-                        <li className='navbar__item'><a href="#" className='navbar__link'>New Cars</a></li>
-                        <li className='navbar__item'><a href="#" className='navbar__link'>Used Cars</a></li>
-                        <li className='navbar__item'><a href="#" className='navbar__link'>Reviews</a></li>
-                        <li className='navbar__item'><a href="#" className='navbar__link'>News</a></li>
+                        <li className='navbar__item'><a href="#" className='navbar__link u-no-wrap'>New Cars</a></li>
+                        <li className='navbar__item'><a href="#" className='navbar__link u-no-wrap'>Used Cars</a></li>
+                        <li className='navbar__item'><a href="#" className='navbar__link u-no-wrap'>Reviews</a></li>
+                        <li className='navbar__item'><a href="#" className='navbar__link u-no-wrap'>News</a></li>
                     </ul>
                     <button className='navbar__btn btn--primary u-border-radius-md'>Login</button>
                 </div>
