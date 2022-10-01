accafb668af0a96d7957283c8d2b9a6a7f5bf8cb
unknown <kimthanh932000@gmail.com>
2022-10-01 16:57:40 +0700

    +

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -11,13 +11,47 @@ const Featured = () => {
         slidesToShow: 3,
         slidesToScroll: 3,
         infinite: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    className: "center",
+                    centerMode: true,
+                    centerPadding: "100px",
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 620,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 570,
+                settings: {
+                    className: "center",
+                    centerMode: true,
+                    centerPadding: "100px",
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 510,
+                settings: {
+                    className: "center",
+                    centerMode: true,
+                    centerPadding: "10px",
+                    slidesToShow: 1,
+                }
+            }
+        ]
     }
 
     return (
         <section className="p-featured">
             <div className='container'>
-                <div className='p-featured-he
-                adline'>
+                <div className='p-featured-headline'>
                     <div>
                         <h2 className="p-featured__title">Our Featured Vehicles.</h2>
                         <p className="p-featured__text">One of our biggest product to be featured<br /> and that has sold out the most.</p>
